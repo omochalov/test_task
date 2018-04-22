71cb76717411c55d4d8ef98a257a6fb35b344779
Unknown <omochalov15@gmail.com>
2018-04-22 15:23:42 +0700

    Mongo: autoreconnect on error/disconnect

--- db/mongo.js
+++ db/mongo.js
@@ -3,7 +3,15 @@ const config = require('config')
 
 let connectionString = config.get('connectionString')
 
-mongoose.connect(connectionString)
+const options = {
+  autoReconnect: true,
+  reconnectTries: Number.MAX_VALUE,
+  reconnectInterval: 5000,
+  poolSize: 10,
+  bufferMaxEntries: 0
+}
+
+mongoose.connect(connectionString, options)
 
 mongoose.connection.on('connected', function () {
   console.log('Mongoose default connection open to ' + connectionString)
@@ -11,10 +19,12 @@ mongoose.connection.on('connected', function () {
 
 mongoose.connection.on('error', function (err) {
   console.log('Mongoose default connection error: ' + err)
+  mongoose.disconnect()
 })
 
 mongoose.connection.on('disconnected', function () {
   console.log('Mongoose default connection disconnected')
+  setTimeout(() => mongoose.connect(connectionString, options), 5000)
 })
 
 process.on('SIGINT', function () {
